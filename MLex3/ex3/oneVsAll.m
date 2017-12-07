9b01845aabb77a995700254347c819716132aff9
bnddnb <bndong@uwaterloo.ca>
2017-12-07 15:47:46 -0500

    modified number prediction for accuracy 94.98%

--- MLex3/ex3/oneVsAll.m
+++ MLex3/ex3/oneVsAll.m
@@ -35,17 +35,21 @@ X = [ones(m, 1) X];
 %       are dealing with large number of parameters.
 %
 
+
+for c = 1:num_labels
      % Set Initial theta
-     initial_theta = zeros(n + 1, 1);
+     %initial_theta = zeros(n + 1, 1);
+     theta = all_theta(c,:);
      
      % Set options for fminunc
      options = optimset('GradObj', 'on', 'MaxIter', 50);
  
      % Run fmincg to obtain the optimal theta
      % This function will return theta and the cost 
-     [theta] = ...
-         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
-                 initial_theta, options);
+     all_theta(c,:) = ...
+         fmincg (@(t)(lrCostFunction(t, X, (y==c), lambda)), ...
+                 theta', options);
+
 
 
 
